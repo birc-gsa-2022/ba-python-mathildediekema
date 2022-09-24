69db5484bc5516f174d14f0d91f8bb4a4383a9c3
Mathilde Diekema <mathildiekema@gmail.com>
2022-09-24 22:08:43 +0200

    Functions fixed

--- src/ba.py
+++ src/ba.py
@@ -1,7 +1,23 @@
 """Module for computing border arrays."""
+import numpy as np
 
+def border_array(x):
 
-def border_array(x: str) -> list[int]:
+    if x == '':
+        return []
+
+    ba = np.zeros(len(x), dtype = int)
+    ba[0] = 0
+    for i in range(1,len(x)):
+        b = ba[i - 1]
+        while b > 0 and x[i] != x[b]:
+            b = ba[b - 1]
+        if x[i] == x[b]:
+            ba[i] = b + 1
+        else:
+            ba[i] = 0
+
+    return ba
     """
     Construct the border array for x.
 
@@ -12,14 +28,22 @@ def border_array(x: str) -> list[int]:
     >>> border_array("")
     []
     """
-    return []  # FIXME
 
 
-def strict_border_array(x: str) -> list[int]:
+
+def strict_border_array(x):
+
+    ba = border_array(x)
+    for i in range(len(x) - 1):
+        if ba[i] > 0 and x[ba[i]] == x[i + 1]:
+            ba[i] = ba[ba[i] - 1]
+    return ba
+
+
     """
     Construct the strict border array for x.
 
-    A struct border array is one where the border cannot
+    A strict border array is one where the border cannot
     match on the next character. If b is the length of the
     longest border for x[:i+1], it means x[:b] == x[i-b:i+1],
     but for a strict border, it must be the longest border
@@ -34,4 +58,5 @@ def strict_border_array(x: str) -> list[int]:
     >>> strict_border_array("")
     []
     """
-    return []  # FIXME
+   
+
